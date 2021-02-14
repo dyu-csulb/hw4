b2ac8a0401d4f566564981d32f490cddb0cb15b6
David Yu <david.yu@outlook.com>
2021-02-13 17:44:54 -0800

    Completed exercise 5

--- js/ex5.js
+++ js/ex5.js
@@ -1,21 +1,57 @@
 function myFunction() {
-    let num1 = '';
-    let message = "";
-    let total = '';
-    num1 = document.getElementById("txtNum1").value;
+    let myArray = [];
+    let values = '';
+    let lowerCaseVal = '';
+    let counter = 0
+    let message = ''
+    values = document.getElementById("txtValues").value;
 
-    let i;
-    for (i = 0; i < 11; i++) {
-       total= Number(num1) * i 
-       message += num1 + ' x '  + i + ' = ' + total + '<br />' 
+    if (values=='') {
+        alert('Please enter some text, a 5 digit value.')
+        document.getElementById("txtValues").value  = '';
+        document.getElementById("txtValues").focus();
     }
+    else if (values.length < 5) {
+        alert('Please enter a 5 digit value.')
+        document.getElementById("txtValues").focus();
+    } 
+    else 
+    {
+        for (let i = 0; i < values.length; i++) {
+            lowerCaseVal = values.toLowerCase().trim();
+            myArray.push(lowerCaseVal[i].trim());
+            switch(true) {
+                case lowerCaseVal[i] == 'a':    
+                    counter +=1;
+                    break;
+                case lowerCaseVal[i] == 'e': 
+                    counter +=1;
+                    break;
+                case lowerCaseVal[i] == 'i': 
+                    counter +=1;
+                    break; 
+                case lowerCaseVal[i] == 'o': 
+                    counter +=1;
+                    break;                        
+                case lowerCaseVal[i] == 'u': 
+                    counter +=1;
+                    break;                   
+              } 
+          }
 
-    document.getElementById("lblMessage").innerHTML  =  message;
-
+          if (myArray[0]==myArray[4] && myArray[1]==myArray[3] ) {
+            message += "'" +  values + "' contains (" + counter + ") vowels and is a palindrome."
+          }
+          else {
+            message += "'" +  values + "' contains (" + counter + ") vowels and is not a palindrome."
+          }
+         
+        document.getElementById("lblMessage").innerHTML  = message
+    }
 }
 
-
 function clearValues() {
-    document.getElementById("txtNum1").value  = '';
+    document.getElementById("txtValues").value  = '';
     document.getElementById("lblMessage").innerHTML  = '';
+    document.getElementById("txtValues").focus();
 }
